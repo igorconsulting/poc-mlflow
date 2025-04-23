21deec2f42fe4cc181f476a5e23e8cd7b8d71b80
Igor-personal <icaetanodiniz@gmail.com>
2025-04-23 16:48:08 -0300

    config parameters

---
+++ config/hyperparams.py
@@ -0,0 +1,9 @@
+PARAMS = {
+    "objective": "multi:softprob",
+    "num_class": 3,
+    "learning_rate": 0.3,
+    "eval_metric": "mlogloss",
+    "colsample_bytree": 0.7,
+    "subsample": 1,
+    "seed": 42,
+}
